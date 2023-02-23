e0a85c8312f32e9cc6ffb47e61ebf308ce36d36a
Anairam Falcon <georgina_gf@live.com.mx>
2023-02-22 23:06:45 -0600

    Inicio del proyecto

---
+++ main.js
@@ -0,0 +1,17 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [-11311680.41959755,2404378.953963324],
+    zoom: 19
+  })
+});
